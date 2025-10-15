e0eb545fd0be33b9471063a6265295f5d9cc00a7
cahenrichs <cahenrichs@gmail.com>
2025-10-15 15:43:19 +0000

    worked on the repl.Made it repeat first word

--- repl.go
+++ repl.go
@@ -1,6 +1,25 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"bufio"
+	"os"
+	"fmt"
+)
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		word := cleanInput(input)
+		firstWord := word[0]
+		if len(word) > 0 {
+			fmt.Printf("Your command was: %s\n", firstWord)
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
