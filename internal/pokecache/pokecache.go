aab54b8bc97f078a225fc8efc753c41168223b31
cahenrichs <cahenrichs@gmail.com>
2025-10-20 20:19:23 +0000

    09 Added the add method

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -5,11 +5,28 @@ import (
 )
 
 type Cache struct {
-	mu	sync.Mutex
-	entries	map[string]cacheEntry
+	mu	*sync.Mutex
+	cache	map[string]cacheEntry
 }
 
 type cacheEntry struct {
 	createdAt	time.Time
 	val	[]byte
+}
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mu: &sync.Mutex{},
+	}
+}
+
+func (c *cache) Add(key string, value []byte) {
+	c.mu.Lock()
+	defer c.mu.Lock()
+
+	c.enties[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:	val,
+	}
 }
