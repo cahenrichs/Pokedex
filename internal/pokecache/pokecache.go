f3d51505c313254f7294edb3882d83cf5a18d22d
cahenrichs <cahenrichs@gmail.com>
2025-10-20 21:57:45 +0000

    10 added the get method and started reapLoop

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -7,6 +7,7 @@ import (
 type Cache struct {
 	mu	*sync.Mutex
 	cache	map[string]cacheEntry
+	interval	time.Duration
 }
 
 type cacheEntry struct {
@@ -18,15 +19,35 @@ func NewCache(interval time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]cacheEntry),
 		mu: &sync.Mutex{},
+		interval:	interval,
 	}
+	go c.reapLoop()
 }
 
-func (c *cache) Add(key string, value []byte) {
+func (c *Cache) Add(key string, value []byte) {
 	c.mu.Lock()
-	defer c.mu.Lock()
+	defer c.mu.Unlock()
 
-	c.enties[key] = cacheEntry{
+	c.cache[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:	val,
 	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	entry, ok := c.cache[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	
 }
